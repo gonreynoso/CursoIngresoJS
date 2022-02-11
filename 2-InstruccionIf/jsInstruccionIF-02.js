a85ba5b7a2a0d81b637e14082ed151d871ea9c32
Gonzalo Reynoso <98622984+gonreynoso@users.noreply.github.com>
2022-02-11 12:43:20 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF-02.js
+++ 2-InstruccionIf/jsInstruccionIF-02.js
@@ -1,7 +1,18 @@
+/*Al ingresar una edad debemos informar solo si la persona es mayor de edad*/
+
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
+	let edadIngresada
+	
+
+	edadIngresada=document.getElementById("txtIdEdad").value;
+	edadIngresada=parseInt(edadIngresada);
+
+	if(edadIngresada>17)
+	{
+		alert("Usted es mayor de edad");
+	}
+
 	
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF-04.js
+++ 2-InstruccionIf/jsInstruccionIF-04.js
@@ -1,7 +1,19 @@
+/*Al ingresar una edad debemos informar si la persona es adolescente,
+edad entre 13 y 17 años (inclusive).*/
+
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
+	let edadIngresada
+	
 
+	edadIngresada=document.getElementById("txtIdEdad").value;
+	edadIngresada=parseInt(edadIngresada);
+
+	if(edadIngresada>13 && edadIngresada<18)
+	{
+		alert("Usted es adolescente");
+	}
+
+	
 
 }//FIN DE LA FUNCIÓN

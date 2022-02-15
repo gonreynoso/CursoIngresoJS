761cbcb17067afb9c4d41a6b05db53594ebe5554
Gonzalo Reynoso <98622984+gonreynoso@users.noreply.github.com>
2022-02-15 12:28:38 -0300

    Update jsInstruccionSwitch-10.js

--- 3-InstruccionSwitch/jsInstruccionSwitch-10.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-10.js
@@ -1,6 +1,74 @@
+/* Ejercicio 10
+una agencia de viajes no pide informar si hacemos viajes a lugares según la estación del año
+donde estemos, informar:
+si "Se viaja" o "No se viaja" a ese lugar.
+
+en Invierno: Solo Bariloche informa "se viaja" los demas destinos "No se viaja"
+
+en Verano: Se viaja a Mar del plata y Cataratas solamente
+
+en Otoño: Se viaja a todos los destinos.
+
+primavera: solo no se viaja a Bariloche
+
+Gonzalo Reynoso*/
+
 function mostrar()
 {
-	var estacionIngresada =txtIdEstacion.value;
-	alert(estacionIngresada);
+	let destinoIngresado;
+	let estacionDelAñoIngresada;
+	let mensaje;
 
+	destinoIngresado=document.getElementById("txtIdDestino").value;
+	estacionDelAñoIngresada=document.getElementById("txtIdEstacion").value;
+	
+	switch(estacionDelAñoIngresada)
+	{
+		case "Invierno":
+			switch(destinoIngresado)
+			{
+				case "Bariloche":
+					mensaje="Se viaja";
+				break;
+				default:
+					mensaje="No se viaja";
+				break;
+			} 
+
+		case "Verano":
+			switch(destinoIngresado)
+			{
+				case "Mar del plata":
+				case "Cataratas":
+					mensaje="Se viaja";
+				break;
+				default:
+					mensaje="No se viaja";
+				break;
+			} 
+
+		case "Otoño":
+			switch(destinoIngresado)
+			{
+				default:
+					mensaje="Se viaja";
+				break;
+			} 
+
+		case "Primavera":
+			switch(destinoIngresado)
+			{
+				case "Bariloche":
+					mensaje="No se viaja";
+				break;
+				default:
+					mensaje="Se viaja";
+				break;
+			} 
+			break;
+	}
+	
+
+	alert(mensaje);
+	
 }//FIN DE LA FUNCIÓN

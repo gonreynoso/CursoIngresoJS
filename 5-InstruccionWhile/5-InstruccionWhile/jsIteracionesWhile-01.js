5431f109037d415828dbf2c907ab7dd999cabffd
Gonzalo Reynoso <98622984+gonreynoso@users.noreply.github.com>
2022-02-18 00:22:16 -0300

    Add files via upload

---
+++ 5-InstruccionWhile/5-InstruccionWhile/jsIteracionesWhile-01.js
@@ -0,0 +1,17 @@
+/*
+al presionar el botón, mostrar 10 repeticiones 
+con números ASCENDENTE, desde el 1 al 10.*/
+function mostrar()
+{
+	let contador;
+
+	contador=0;
+	while(contador<10)
+	{
+		contador=contador +1;
+		alert(contador);
+		
+		
+	}
+	
+}//FIN DE LA FUNCIÓN
---
+++ 5-InstruccionWhile/5-InstruccionWhile/jsIteracionesWhile-02.js
@@ -0,0 +1,17 @@
+/*
+al presionar el botón, mostrar 10 repeticiones 
+con números DESCENDENTES, desde el 10 al 1.*/
+function mostrar()
+{
+	let contador;
+
+	contador=10;
+	while(contador>0)
+	{
+		contador=contador-1;
+		alert(contador);
+		
+		
+	}
+
+}//FIN DE LA FUNCIÓN
